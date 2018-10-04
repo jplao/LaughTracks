592f6dc316a50c69f62a148c853233165e310d84
Jenn Lao <jenn.p.lao@gmail.com>
2018-10-04 13:14:48 -0600

    adds comedians and specials

--- app/models/special.rb
+++ app/models/special.rb
@@ -1,5 +1,5 @@
 class Special < ActiveRecord::Base
-  validates_presence_of :name, :run_time, :thumbnail
+  validates_presence_of :name, :run_time
 
   belongs_to :comedian
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,56 @@
-comedian1 = Comedian.create(name: "Louis C.K", age: 51, city: "Washington D.C")
+louis = Comedian.create(name: "Louis C.K", age: 51, city: "Washington D.C")
+louis.specials.create(name: "Louis C.K. 2017", run_time: 74)
+louis.specials.create(name: "Louis C.K.: Live at the Comedy Store", run_time: 66)
+louis.specials.create(name: "Louis C.K. Oh My God", run_time: 58)
+
+dave = Comedian.create(name: "Dave Chappelle", age: 45, City: "Washington D.C")
+dave.specials.create(name: "Dave Chapelle: Equanimity", run_time: 60)
+dave.specials.create(name: "Dave Chapelle: The Bird Revelation", run_time: 49)
+dave.specials.create(name: "Dave Chapelle: For What It's Worth", run_time: 60)
+
+aziz = Comedian.create(name: "Aziz Ansari", age: 35, City: "Columbia, SC")
+aziz.specials.create(name: "Aziz Ansari: Buried Alive", run_time: 80)
+aziz.specials.create(name: "Aziz Ansari: Dangerously Delicious", run_time: 61)
+aziz.specials.create(name: "Aziz Ansari: Intimate Moments for a Sensual Evening", run_time: 56)
+
+david = Comedian.create(name: "David Cross", age: 54, City: "Atlanta, GA")
+david.specials.create(name: "David Cross: Making America Great Again", run_time: 74)
+david.specials.create(name: "David Cross: Bigger & Blackerer", run_time: 90)
+david.specials.create(name: "David Cross: Let America Laugh", run_time: 93)
+
+patton = Comedian.create(name: "Patton Oswalt", age: 49, City: "Portsmouth, VA")
+patton.specials.create(name: "Patton Oswalt: Annihilation ", run_time: 66)
+patton.specials.create(name: "Patton Oswalt: Talking for Clapping", run_time: 65)
+patton.specials.create(name: "Patton Oswalt: Tragedy Plus Comedy Equals Time", run_time: 59)
+
+sarah = Comedian.create(name: "Sarah Silverman", age: 48, city: "Bedford, NH")
+sarah.specials.create(name: "Sarah Silverman: A Speck of Dust", run_time: 71)
+sarah.specials.create(name: "Sarah Silverman: We Are Miracles", run_time: 53)
+
+bill = Comedian.create(name: "Bill Burr", age: 50, city: "Canton, MA")
+bill.specials.create(name: "Bill Burr: Walk Your Way Out", run_time: 77)
+bill.specials.create(name: "Bill Burr: I'm Sorry You Feel That Way", run_time: 80)
+bill.specials.create(name: "Bill Burr: Let It Go", run_time: 65)
+
+chris = Comedian.create(name: "Chris Rock", age: 53, city: "Andrews, SC")
+chris.specials.create(name: "Chris Rock: Tamborine", run_time: 64)
+chris.specials.create(name: "Chris Rock: Kill the Messenger - London, New York, Johannesburg", run_time: 79)
+chris.specials.create(name: "Chris Rock: Never Scared", run_time: 80)
+
+sandra = Comedian.create(name: "Sandra Bernhard", age: 63, city: "Flint, MI")
+sandra.specials.create(name: "Sandra Bernhard: I'm Still Here... Damn It!", run_time: 90)
+sandra.specials.create(name: "Women of the Night III", run_time: 60)
+
+maria = Comedian.create(name: "Maria Bamford", age: 48, city: "Port Hueneme, California")
+maria.specials.create(name: "Maria Bamford: Old Baby", run_time: 64)
+maria.specials.create(name: "Maria Bamford: The Special Special Special!", run_time: 49)
+
+kevin_h = Comedian.create(name: "Kevin Hart", age: 39, city: "Philadelphia, PA")
+kevin_h.specials.create(name: "Plastic Cup Boyz", run_time: 82)
+kevin_h.specials.create(name: "Kevin Hart: I'm a Grown Little Man", run_time: 73)
+kevin_h.specials.create(name: "Kevin Hart: Seriously Funny", run_time: 72)
+
+kevin_s = Comedian.create(name: "Kevin Smith", age: 48, city: "Red Bank, NJ")
+kevin_s.specials.create(name: "Kevin Smith: Silent But Deadly", run_time: 57)
+kevin_s.specials.create(name: "Kevin Smith: Burn in Hell", run_time: 89)
+kevin_s.specials.create(name: "Kevin Smith: Too Fat for 40!", run_time: 120)
