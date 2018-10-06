6a63066df35a1ee58c0d0d69912ef3b7eafdbca4
Jenn Lao <jenn.p.lao@gmail.com>
2018-10-06 06:02:08 -0600

    refactors statistics

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -3,11 +3,10 @@ class LaughTracksApp < Sinatra::Base
   get '/comedians' do
     if params[:age]
       @comedians = Comedian.where(age: params[:age])
-      @specials = Special.where(comedian_id: @comedians.ids)
     else
       @comedians = Comedian.all
-      @specials = Special.all
     end
+    @specials = Special.where(comedian_id: @comedians.ids)
     erb :"comedians/index"
   end
 
