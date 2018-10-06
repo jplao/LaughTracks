a411612f0152b56da0edd32f1b97685c3349b2d5
Jenn Lao <jenn.p.lao@gmail.com>
2018-10-06 05:52:47 -0600

    statistics working for dynamic search

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -3,7 +3,7 @@ class LaughTracksApp < Sinatra::Base
   get '/comedians' do
     if params[:age]
       @comedians = Comedian.where(age: params[:age])
-      @specials = Special.all
+      @specials = Special.where(comedian_id: @comedians.ids)
     else
       @comedians = Comedian.all
       @specials = Special.all
